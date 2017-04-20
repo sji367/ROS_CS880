cd6c001fa60146b054fc771f0766cf4beedaa702
Sam Reed <sji367@wildcats.unh.edu>
2017-04-20 17:17:17 -0400

    frontier

--- reed_final/scripts/frontier_exploration.py
+++ reed_final/scripts/frontier_exploration.py
@@ -98,6 +98,42 @@ class Frontier_Based_Exploration():
         """ Converts the x,y coordinates into a row-major index for the map"""
         return y*self.ogrid_sizeY + x
         
+    def distanceFomula(self, x1,y1, x2,y2):
+         return np.sqrt(np.square(x2 - x1) + np.square(y2-y1))
+         
+    def calcUtility(self, centroidX, centroidY, centroidLength):
+        """ Calculate the utility of the frontier's centroid using a combo of
+            the distance to the centroid and the length of the centroid.
+        """
+        dist_weight = 1
+        length_weight = 1
+        dist = self.distanceFomula(centroidX, centroidY, self.position.x, self.position.y)
+        util = (length_weight*centroidLength)/(dist_weight*dist)
+        return util
+        
+    def calcUtil_dist(self, centroidX, centroidY):
+        """ Calculate the utility of the frontier's centroid using just the
+            distance to the centroid. 
+        """
+        dist = self.distanceFomula(centroidX, centroidY, self.position.x, self.position.y)
+        return dist
+        
+    def setOccupancy(self, index, prior, Free):
+        """ """
+        # If there is no previous data, then use a prior of 0.5 (Beta distribution)
+        if prior == -1:
+            prior = 0.5
+        
+        # Calculate eta
+        eta = self.p_measurement_given_occupied*prior + self.p_measurement_given_notOccupied*(1-prior)
+
+        if Free:
+            prob_occupancy = eta*self.p_measurement_given_occupied*prior
+#            p = self.p_measurement_given_occupied
+        else:
+            prob_occupancy = eta*self.p_measurement_given_notOccupied*(1-prior)
+#            p = self.p_measurement_given_notOccupied
+        self.map[index] = prob_occupancy#prior+np.log()
     
     def updateNeighbors(self, scanX, scanY):
         """ Update the free occupancy grid cells between the robot and the   
@@ -105,25 +141,28 @@ class Frontier_Based_Exploration():
         """
         dx = scanX - self.position.x
         dy = scanY - self.position.y
+        prevX = 0
+        prevY = 0
         # Need to check that the path does not pass an object
         JumpCells=2*max(abs(dx),abs(dy))-1
         for K in range(1,JumpCells):
             # intermediate positions
-            YPOS=int(round(K*1.0*dy/JumpCells));
-            XPOS=int(round(K*1.0*dx/JumpCells));
-            # update the map
-            row_major_index = self.mapIndex(self.position.x+XPOS, self.position.y+YPOS)
-            self.map[row_major_index] = 0
+            YPOS=int(round(K*1.0*dy/JumpCells))
+            XPOS=int(round(K*1.0*dx/JumpCells))
+            if (prevX != XPOS) or (prevY != YPOS):
+                # update the map
+                row_major_index = self.mapIndex(self.position.x+XPOS, self.position.y+YPOS)
+                self.setOccupancy(row_major_index, self.map[row_major_index], True)
     
     def updateMap(self):
         """ This function updates the occupancy grid cells from the """
         laser_data=self.scan.ranges
-        cur_angle = -self.scan.angle_min
+        cur_angle = normalize_angle(self.scan.angle_min+self.position.theta)
         for i in range(len(laser_data)):
             if not np.isnan(laser_data[i]):
                 # Update the current angle to reflect the angle of the laser 
                 #  scan.
-                cur_angle += self.scan.angle_increment*i
+                cur_angle = normalize_angle(self.scan.angle_increment*i + cur_angle)
                 
                 # Determine the location of the obstacle found by the laser scan
                 scanX = laser_data[i]*np.cos(cur_angle)
@@ -133,63 +172,135 @@ class Frontier_Based_Exploration():
                 
                 # Update the map between the obstacle and the robot
                 self.updateNeighbors(scanX_grid, scanY_grid)
-                self.map[self.mapIndex(scanX_grid, scanY_grid)] = 1
+                
+                # Update the map to include the obstacle
+                row_major_index = self.mapIndex(scanX_grid, scanY_grid)
+                self.setOccupancy(row_major_index, self.map[row_major_index], False)
+                
+    def findFrontier(self):
+        """ This function cycles through each cell in the map and finds all 
+            cells that are part of the frontier. The frontier is defined as 
+            regions on the border of empty space and unknown space.
+        """
+        # Cycle through all points in the map. When a unknown
+        pass
         
     def calc_centroid(self, points):
-        """ This function takes in a set of points and finds its centroid. """
-        x_c = 0
-        y_c = 0
-        count = 0
-        for index in len(points):
-            x = points[0]
-            y = points[1]
-            x_c=x_c+ x
-            y_c=y_c+ y
-            count+=1
-        x_c=x_c/count
-        y_c=y_c/count
-    
+        """ This function takes in a set of points and finds its centroid.
+            
+            Input:
+                points - 2D array where the each row contains the X,Y location 
+                    of the frontier cell
+        """
+        if len(points) == 0:
+            return
+        num_Rows = len(points[0])
+        x_c, y_c = np.sum(points, axis=0)
+        x_c /= int(np.round(1.0*num_Rows))
+        y_c /= int(np.round(1.0*num_Rows))
+        return x_c, y_c, self.calcUtil_dist(x_c, y_c)
+#        x_c = 0
+#        y_c = 0
+#        length = 0
+#        count = 0
+#        prev_x = 0
+#        prev_y = 0
+#        for index in range(len(points)):
+#            x = points[index][0]
+#            y = points[index][1]
+#            # This length can be incorrect if the previous point in the array 
+#            #  "points" is not the closest point to the current point
+#            if index >0:
+#                length += self.distanceFomula(prev_x,prev_y, x,y)
+#            x_c += x
+#            y_c += y
+#            # Store the previous x and y locations of the frontier
+#            prev_x = x
+#            prev_y = y
+#            # Increment the counter
+#            count+=1
+#        if count == 0:
+#            return
+#        elif count == 1:
+#            length = 1
+#        x_c=x_c/count
+#        y_c=y_c/count
+#        util = self.calcUtility(x_c,y_c, length)
+#        return x_c, y_c, util
+        
+    def pickBestCentroid(self, frontiers):
+        """ Takes in all frontiers (as a 3D array) and """
+        centroidX = []
+        centroidY = []
+        utility = []
+        for i in range(len(frontiers)):
+            x_c, y_c, distance_c = self.calc_centroid(frontiers[i])
+            centroidX.append(x_c)
+            centroidY.append(y_c)
+            utility.append(distance_c)
+        # Determine which centroid is the closest 
+        index = np.argmax(utility)
+        return centroidX[index], centroidY[index]
+        
+        
     def run(self):
         """ Runs the frontier based exploration. """
         # IN THIS LOOP ROS SENDS AND RECEIVES  
         while not rospy.is_shutdown():
-            self.cmd_pose.publish(self.new_pose)
+            #self.cmd_pose.publish(self.new_pose)
+            if find_new_centroid:
+                frontiers = self.findFrontiers()
+                centroidX, centroidY = self.pickBestCentroid(frontiers)
+                self.move2Centroid(centroidX, centroidY)
+            else:
+                while (len(self.new_scans)>0):
+                    self.updateMap()
             self.r.sleep()
                 
-    def __init__(self, init_sizeX = 100, init_sizeY = 100, grid_size=0.05):
+    def __init__(self):
         """ Initialize """
         rospy.init_node('Frontier_exploration')
-        #rospy.on_shutdown(self.shutdown())
-        self.ogrid_sizeX = init_sizeX
-        self.ogrid_sizeY = init_sizeY
+        # Get the parameters for the grid
+        self.ogrid_sizeX = rospy.get_param('x_size', 200)
+        self.ogrid_sizeY = rospy.get_param('y_size', 200)
+        self.grid_size = rospy.get_param('grid_size', 0.05) # in meters/cell
+         # Sensor reliability
+        self.p_measurement_given_occupied = rospy.get_param('p_z|occ',0.9)
+        self.p_measurement_given_notOccupied = rospy.get_param('p_z|notOcc',0.3)
         
+        # Initialize some varables to store the objects to be published/subscribed
         self.origin = Position()
         self.position = Position()
         self.pos = PoseStamped()
         self.odom = Odometry()
-        
-        self.map = [-1]*init_sizeX*init_sizeY # row-major order
-        self.grid_size = grid_size # in meters/cell
-        
-        # loop rate
-        self.r = rospy.Rate(50)
         
         self.current_map = OccupancyGrid()
         self.meta = MapMetaData()
         self.scan = LaserScan()
         self.new_pose = PoseStamped()
         
-        # publisher for twist messages
-        #self.sub_map = rospy.Subscriber('/map', OccupancyGrid, self.costmap_callback)
+        # Initialize the map as unknown (-1)
+        self.map = [-1]*self.ogrid_sizeX*self.ogrid_sizeY # row-major order
+        
+       
+        
+        # loop rate
+        self.r = rospy.Rate(50)
+        
+        # publishers for OccupancyGrid and move base messages
         self.pub_map = rospy.Publisher('/map', OccupancyGrid, queue_size=100)
         self.cmd_pose = rospy.Publisher('/move_base_simple/goal', PoseStamped, queue_size=100)
-        # subscribe to odometry messages
+        
+        # subscribers for odometry and position (/move_base/feedback) messages
         self.odom_sub = rospy.Subscriber("/odom", Odometry, self.odom_callback)
         self.feedback_sub = rospy.Subscriber("/move_base/feedback", PoseStamped, self.feedback_callback)
+        self.scan_sub = rospy.Subscriber("/scan", LaserScan, self.scan_callback)
+        #self.sub_map = rospy.Subscriber('/map', OccupancyGrid, self.costmap_callback)
         
         self.run()
         
         return
 
-l = Frontier_Based_Exploration()
+if  __name__=="__main__":
+    l = Frontier_Based_Exploration()
 
