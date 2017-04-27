aa17d633abebf132684580f6f9c0860078b51dfb
Sam Reed <sji367@wildcats.unh.edu>
2017-04-27 12:33:43 -0400

    today

--- reed_final/scripts/frontier_exploration.py
+++ reed_final/scripts/frontier_exploration.py
@@ -50,14 +50,14 @@ class Frontier_Based_Exploration():
         
     def costmap_callback(self, map_msg):
         """ Callback to handle Map messages. """
-        print "Got new MSG!"
-        self.new_msg = True
-        self.current_map.data = map_msg.data 
+        print map_msg.info
+        self.new_msg = True 
         
         self.meta.resolution= map_msg.info.resolution
-        self.meta.height = map_msg.info.height
-        self.meta.width = map_msg.info.width
-        self.meta.origin = map_msg.info.origin
+        self.meta.height = map_msg.height
+        self.meta.width = map_msg.width
+        self.meta.origin.position.x = map_msg.origin.position.x
+        self.meta.origin.position.y = map_msg.origin.position.y
     
     def scan_callback(self, scan_msg):
         self.scan.ranges = scan_msg.ranges
@@ -110,12 +110,13 @@ class Frontier_Based_Exploration():
         
     def grid2xy(self, gridX, gridY):
         """ Converts the grid coordinates to the local X,Y. """
-        x = gridX*self.grid_size+self.origin.x+self.grid_size/2.0
-        y = gridY*self.grid_size+self.origin.y+self.grid_size/2.0 
+        x = (gridX-self.origin.x)*self.grid_size
+        y = (gridY-self.origin.y)*self.grid_size
         return x,y
         
     def xy2mapIndex(self, x,y):
-        """ Converts the x,y coordinates into a row-major index for the map"""
+        """ Converts the x,y grid coordinates into a row-major index for the 
+            map. """
         if x>self.ogrid_sizeX or y>self.ogrid_sizeY:
             return self.ogrid_sizeX * self.ogrid_sizeY -1
         else:
@@ -164,6 +165,7 @@ class Frontier_Based_Exploration():
             prob_occupancy = eta*self.p_measurement_given_notOccupied*(prior)
         else:
             prob_occupancy = eta*self.p_measurement_given_occupied*prior
+#            print prob_occupancy, index
             
         self.current_map.data[index] = int(prob_occupancy*100)
     
@@ -171,7 +173,6 @@ class Frontier_Based_Exploration():
         """ Update the free occupancy grid cells between the robot and the   
             obstacle found using the laser scan.
         """
-        
         camX, camY = self.xy2grid(camera_pos.x, camera_pos.y)
         robotX, robotY = self.xy2grid(self.position.x, self.position.y)
         
@@ -187,7 +188,7 @@ class Frontier_Based_Exploration():
             XPOS=int(round(K*1.0*dx/JumpCells))
             if (prevX != XPOS) or (prevY != YPOS):
                 # update the map
-                row_major_index = self.xy2mapIndex(robotX+XPOS, robotY+YPOS)
+                row_major_index = self.xy2mapIndex(camX+XPOS, camY+YPOS)
                 self.setOccupancy(row_major_index, self.current_map.data[row_major_index], True)
     
     def updateMap(self):
@@ -195,9 +196,14 @@ class Frontier_Based_Exploration():
         camera_pos = Position()     
         camera_pos = self.getCameraPositon()
         
+        # The Current position of the robot should be free
+        camGridX,camGridY=self.xy2grid(camera_pos.x, camera_pos.y)
+        cam_index= self.xy2mapIndex(camGridX,camGridY)
+        self.setOccupancy(cam_index, self.current_map.data[cam_index], True)
+        
         laser_data=self.scan.ranges
         cur_angle = normalize_angle(self.scan.angle_min+camera_pos.theta)
-        
+        print 'update',len(laser_data)
         for i in range(len(laser_data)):
             if not np.isnan(laser_data[i]):            
                 # Update the current angle to reflect the angle of the laser 
@@ -215,11 +221,25 @@ class Frontier_Based_Exploration():
                     
                 # Update the map to include the obstacle
                 row_major_index = self.xy2mapIndex(scanX_grid, scanY_grid)
+#                print row_major_index
                 self.setOccupancy(row_major_index, self.current_map.data[row_major_index], False)           
         self.publishNewMap()
         
     def publishNewMap(self):
         self.current_map.header.frame_id = 'map'
+#        originX = self.origin.x*self.grid_size
+#        originY = self.origin.y*self.grid_size
+#        cur_pos= Position()
+##        cur_pos= self.get_current_position()
+##        print cur_pos.x, cur_pos.y
+##        print cur_pos.x-originX,  cur_pos.y-originY
+        dif_in_middleX = self.origin.x*self.grid_size-self.meta.width*self.meta.resolution
+        dif_in_middleY = self.origin.y*self.grid_size-self.meta.height*self.meta.resolution
+#        print dif_in_middleX, dif_in_middleY
+        self.current_map.info.origin.position.x = -dif_in_middleX
+        self.current_map.info.origin.position.y = -dif_in_middleY
+        self.current_map.info.origin.orientation.w = 1
+#        print self.meta.origin.position.x, self.meta.origin.position.y
         self.pub_map.publish(self.current_map)
                 
     def blogDetection(self):
@@ -459,9 +479,12 @@ class Frontier_Based_Exploration():
         origin.y = self.ogrid_sizeY/2
         origin.theta = 0
         
-        self.current_map.info.origin.position.x = -1#self.ogrid_sizeX/2
-        self.current_map.info.origin.position.y = -1#self.ogrid_sizeY/2
-        self.current_map.info.origin.orientation.w = 1
+#        trans, rot = self.listener.lookupTransform('map', 'base_link', rospy.Time(0))
+        self.current_map.info.origin = self.meta.origin
+#        self.current_map.info.origin.position.x = origin.x*self.grid_size
+#        self.current_map.info.origin.position.y = origin.y*self.grid_size
+#        self.current_map.info.origin.orientation.z = rot[2]
+#        self.current_map.info.origin.orientation.w = rot[3]
         self.current_map.info.height = self.ogrid_sizeX
         self.current_map.info.width = self.ogrid_sizeY
         self.current_map.info.resolution = self.grid_size
@@ -497,12 +520,15 @@ class Frontier_Based_Exploration():
         start = rospy.Time.now().to_sec()
         # IN THIS LOOP ROS SENDS AND RECEIVES  
         while not rospy.is_shutdown():
+            start = rospy.Time.now().to_sec()
             self.updateMap()
-            if (rospy.Time.now().to_sec()-start) > 1:
-                frontiers = self.getFrontier()
-                centroidX, centroidY = self.pickBestCentroid(frontiers)
-                print centroidX, centroidY
-            return
+            print rospy.Time.now().to_sec()-start
+#            if (rospy.Time.now().to_sec()-start) > 1:
+#                frontiers = self.getFrontier()
+#                centroidX, centroidY = self.pickBestCentroid(frontiers)
+#                print centroidX, centroidY
+#            if (rospy.Time.now().to_sec()-start) > 2: 
+#                return
             
 #            if self.new_msg:
 #                print 'inside'
@@ -535,9 +561,9 @@ class Frontier_Based_Exploration():
         self.listener = tf.TransformListener()        
         
         # Get the parameters for the grid
-        self.ogrid_sizeX = rospy.get_param('x_size', 40)
-        self.ogrid_sizeY = rospy.get_param('y_size', 40)
-        self.grid_size = rospy.get_param('grid_size', 0.5) # in meters/cell (25cm)
+        self.ogrid_sizeX = rospy.get_param('x_size', 250)
+        self.ogrid_sizeY = rospy.get_param('y_size', 250)
+        self.grid_size = rospy.get_param('grid_size', 0.15) # in meters/cell (25cm)
         
         # Sensor Meta data
         self.max_range = rospy.get_param('max_range',8.0)
@@ -556,7 +582,6 @@ class Frontier_Based_Exploration():
         self.meta = MapMetaData()
         self.scan = LaserScan()
         self.new_pose = PoseStamped()
-        self.origin = self.setOrigin()
         
         # Initialize the map as unknown (-1)
         self.current_map.data = [-1]*self.ogrid_sizeX*self.ogrid_sizeY # row-major order
@@ -571,12 +596,13 @@ class Frontier_Based_Exploration():
         
         # subscribers for odometry and position (/move_base/feedback) messages
 #        self.odom_sub = rospy.Subscriber("/odom", Odometry, self.odom_callback)
-#        self.feedback_sub = rospy.Subscriber("/move_base/feedback", PoseStamped, self.feedback_callback)
+#        self.feedback_sub = rospy.Subscriber("/move_base/status", PoseStamped, self.feedback_callback)
         self.scan_sub = rospy.Subscriber("/scan", LaserScan, self.scan_callback)
-#        self.sub_map = rospy.Subscriber('/map', OccupancyGrid, self.costmap_callback)
+        self.sub_map = rospy.Subscriber('/map/info', MapMetaData, self.costmap_callback)
         
         self.listener.waitForTransform('/map', '/base_link', rospy.Time(0), rospy.Duration(1))
         
+        self.origin = self.setOrigin()
         #self.listener.waitForTransform('/scan', '/base_link', rospy.Time(0), rospy.Duration(1)) # Need to test this
         self.run()
         
